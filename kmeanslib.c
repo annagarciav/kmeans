611671848e3e0b0caac1ce08dbaa61d589eaefe6
Anna <116257068+annagarciav@users.noreply.github.com>
2024-05-08 17:29:19 +0200

    pragmas SIN EJECUTAR

---
+++ kmeanslib.c
@@ -0,0 +1,280 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include "kmeanslib.h"
+#include <omp.h>
+
+
+/*
+ * Read File function
+ * 
+ * mImage->pixels = malloc(mImage->length * sizeof(rgb));
+ *
+ */
+int read_file(char *name, image* mImage)
+{
+	if((mImage->fp=fopen(name, "r")) == NULL) {
+		printf("No fue posible abrir el BMP.\n");
+		exit(6);
+	}
+	if(fseek(mImage->fp, 18, SEEK_SET) != 0) {
+		printf("Error fseek.\n");
+		exit(6);
+	}
+	if(fread(&mImage->width, sizeof(mImage->width), 1, mImage->fp) != 1) {
+		printf("Error Writing File.\n");
+		exit(6);
+	}
+	if(fseek(mImage->fp, 22, SEEK_SET) != 0) {
+		printf("Error fseek.\n");
+		exit(6);
+	}
+	if(fread(&mImage->height, sizeof(mImage->height), 1, mImage->fp) != 1) {
+		printf("Error Reading H File.\n");
+		exit(6);
+	}
+ 
+	mImage->length = (mImage->width * mImage->height);
+ 
+	mImage->pixels  = malloc(mImage->length * sizeof(rgb));
+
+	if(mImage->pixels == NULL){
+		printf("Not enough memory.\n");
+		return 6;
+	}
+
+	if(fseek(mImage->fp, 0, SEEK_SET) != 0){
+		printf("Error fseek.\n");
+		return 6;
+	}
+
+	if(fread(&mImage->header, sizeof(uint8_t), sizeof(mImage->header), mImage->fp) != sizeof(mImage->header)){
+		printf("Error reading header.\n");
+		return 6;
+	}
+
+	int i,c;
+ 
+	for(i = 0; i < mImage->length; i++){
+		c = getc(mImage->fp);
+		if(c == EOF){
+			printf("Error Reading File.\n");
+			return 5;
+		}
+		mImage->pixels[i].b = c;
+		c = getc(mImage->fp);
+		if(c == EOF){
+			printf("Error Reading File.\n");
+			return 5;
+		}
+		mImage->pixels[i].g = c;
+		c = getc(mImage->fp);
+		if(c == EOF){
+			printf("Error Reading File.\n");
+			return 5;
+		}
+		mImage->pixels[i].r = c;
+	}
+ 
+	if(fclose(mImage->fp) != 0){
+		printf("Error cerrando fichero.\n");
+		return 1;
+	}
+	return (0);
+}
+
+int write_file(char *name, image *mImage, cluster*centroids, uint8_t k){
+	FILE *fp;
+	int i,j;
+	uint8_t closest;
+	rgb pixel;
+
+	if((fp=fopen(name, "w")) == NULL){
+			printf("Cannot create output file, do you have write permissions for current directory?.\n");
+			return 1;
+		}
+		if(fwrite(mImage->header, sizeof(uint8_t), sizeof(mImage->header), fp) != sizeof(mImage->header)){
+			printf("Error writing BMP.\n");
+			return 1;
+		}
+		for(i = 0; i < mImage->width; i++){
+			for(j = 0; j < mImage->height; j++){
+				closest = find_closest_centroid(&mImage->pixels[i * mImage->height + j], centroids, k);
+				pixel.r = centroids[closest].r;
+				pixel.g = centroids[closest].g;
+				pixel.b = centroids[closest].b;
+				
+				if(fwrite(&pixel, sizeof(uint8_t), 3, fp) != 3){
+					printf("Error writing BMP.\n");
+					return 1;
+				}
+			}
+		}
+		if(fclose(fp) != 0){
+			printf("Error closing file.\n");
+			return 1;
+		}
+		if((fp=fopen("clusters.txt", "w")) == NULL){
+			printf("Clusters.txt cannot be created. Do you have space | owner in current directory?\n");
+			return 1;
+		}
+		
+		fprintf(fp, "Red\tGreen\tBlue\n");
+
+		for(j = 0; j < k; j++){
+			fprintf(fp, "%d\t%d\t%d\n", centroids[j].r, centroids[j].g, centroids[j].b);
+		}
+
+		if(fclose(fp) != 0){
+			printf("Error closing fichero.\n");
+			return 1;
+		}
+}
+
+
+/*
+ * Compute Checksum
+ * Checksum is the total sum of the product (r,g,b) of the centroids final value.
+ *
+ */
+uint32_t getChecksum(cluster* centroids, uint8_t k){
+  uint32_t i,j;
+  uint32_t sum = 0;
+  
+  for (i = 0; i < k; i++)
+  {
+    printf("Centroide %u : R[%u]\tG[%u]\tB[%u]\n", i, centroids[i].r, centroids[i].g, centroids[i].b);
+    sum += (centroids[i].r * centroids[i].g * centroids[i].b);
+  }
+  return sum;
+}
+
+/*
+ * Return the index number of the closest centroid to a given pixel (p)
+ * input @param: p 				--> pixel pointer
+ * input @param: centroids 		--> cluster array pointer
+ * input @param: num_clusters	--> amount of centroids in @param: centroids
+ *
+ * output: uint8_t --> Index of the closest centroid
+ *
+ */
+uint8_t find_closest_centroid(rgb* p, cluster* centroids, uint8_t num_clusters){
+	uint32_t min = UINT32_MAX;
+	//uint32_t dis[num_clusters];
+	uint8_t closest = 0, j;
+	int16_t diffR, diffG, diffB;
+	uint32_t aux; // !!!!!!!!!!!!!!!!!!
+
+	for(j = 0; j < num_clusters; j++) 
+	{
+		diffR = centroids[j].r - p->r;
+		diffG = centroids[j].g - p->g;
+		diffB = centroids[j].b - p->b; 
+		// No sqrt required.
+		aux = diffR*diffR + diffG*diffG + diffB*diffB;
+		
+		//if(dis[j] < min) 
+		if(aux < min)
+		{
+			min = aux;
+			closest = j;
+		}
+	}
+	return closest;
+}
+
+/*
+ * Main function k-means	
+ * input @param: K 			--> number of clusters
+ * input @param: centroides --> the centroids
+ * input @param: num_pixels --> number of total pixels
+ * input @param: pixels 	--> pinter to array of rgb (pixels)
+ */
+void kmeans(uint8_t k, cluster* centroides, uint32_t num_pixels, rgb* pixels){	
+	uint8_t condition, changed, closest;
+	uint32_t i, j, random_num;
+	
+	uint32_t* red = malloc(k* sizeof(uint32_t));
+	uint32_t* green = malloc(k* sizeof(uint32_t));
+	uint32_t* blue = malloc(k* sizeof(uint32_t));
+	uint32_t* points = malloc(k* sizeof(uint32_t));
+	
+	printf("STEP 1: K = %d\n", k);
+	k = MIN(k, num_pixels);
+	
+	// Init centroids	
+	printf("STEP 2: Init centroids\n");
+	for(i = 0; i < k; i++) 
+	{
+		random_num = rand() % num_pixels;
+		centroides[i].r = pixels[random_num].r;
+		centroides[i].g = pixels[random_num].g;
+		centroides[i].b = pixels[random_num].b;
+	}
+
+	// K-means iterative procedures start
+	printf("STEP 3: Updating centroids\n\n");
+	i = 0;
+	do 
+  	{
+		// Reset centroids
+	//! transferencia de dades (copies) entre la memoria del HOST (CPU) i la del dispositiu (GPU)
+	#pragma acc data copy(centroides[0:k], pixels[0:num_pixels]) \ copyout(red[0:k], green[0:k], blue[0:k], points[0:k])
+	{
+		#pragma acc parallel loop present(centroides[0:k])
+		for(j = 0; j < k; j++) 
+    		{
+			centroides[j].media_r = 0;
+			centroides[j].media_g = 0;
+			centroides[j].media_b = 0;
+			centroides[j].num_puntos = 0;
+		
+			red[j] = 0;
+			green[j] = 0;
+			blue[j] = 0;
+			points[j] = 0;
+		}
+   
+		// Find closest cluster for each pixel
+		#pragma acc parallel loop present(centroides[0:k], pixels[0:num_pixels], red[0:k], green[0:k], blue[0:k], points[0:k])
+		for(j = 0; j < num_pixels; j++) 
+    	{
+			closest = find_closest_centroid(&pixels[j], centroides, k); // !! es el cuello botella! paralelizar la funcion
+			//centroides[closest].media_r += pixels[j].r;
+			//centroides[closest].media_g += pixels[j].g;
+			//centroides[closest].media_b += pixels[j].b;
+			// centroides[closest].num_puntos++;
+			red[closest] += pixels[j].r;
+			green[closest] += pixels[j].g;
+			blue[closest] += pixels[j].b;
+			points[closest] += 1;
+		}
+
+		// Update centroids & check stop condition
+		condition = 0;
+		#pragma acc parallel loop reduction(||: condition)
+		for(j = 0; j < k; j++) 
+		{
+			if(points[j] > 0) 
+			{
+			centroides[j].media_r = red[j] / points[j];
+			centroides[j].media_g = green[j] / points[j];
+			centroides[j].media_b = blue[j] / points[j];
+			changed = centroides[j].media_r != centroides[j].r || centroides[j].media_g != centroides[j].g || centroides[j].media_b != centroides[j].b;
+			condition = condition || changed;
+			centroides[j].r = centroides[j].media_r;
+			centroides[j].g = centroides[j].media_g;
+			centroides[j].b = centroides[j].media_b;
+			}
+		}
+	}
+	i++;
+	} while(condition);
+	printf("Number of K-Means iterations: %d\n\n", i);
+}
